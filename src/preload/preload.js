1d880dac24d3ec8ac775d2a58e4a844c60fe92c4
Maliuk Oleksandr <omaliuk@gerc.ua>
2026-05-28 13:56:31 +0300

    electron build

--- src/preload/preload.js
+++ src/preload/preload.js
@@ -4,15 +4,15 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
 
-  getHomePath: () => ipcRenderer.invoke('get-home-path'),
+  getHomePath: () => ipcRenderer.invoke('get-home-path'), // not used yet, but will be in the future
 
-  getQuickAccess: () => ipcRenderer.invoke('get-quick-access'),
+  getQuickAccess: () => ipcRenderer.invoke('get-quick-access'), // not used yet, but will be in the future
 
-  getSystemDrives: () => ipcRenderer.invoke('get-system-drives'),
+  getSystemDrives: () => ipcRenderer.invoke('get-system-drives'), // not used yet, but will be in the future
 
-  explorePath: (targetPath) => ipcRenderer.invoke('explore-path', targetPath),
+  explorePath: (targetPath) => ipcRenderer.invoke('explore-path', targetPath), // not used yet, but will be in the future
 
-  openInShell: (targetPath) => ipcRenderer.invoke('open-in-shell', targetPath),
+  openInShell: (targetPath) => ipcRenderer.invoke('open-in-shell', targetPath), // not used yet, but will be in the future
 
   openModal: (type) => ipcRenderer.invoke('open-loop-modal', type)
 });
